86492e1ea4031e844df659d48a45bb095f0cebe1
Allen Zheng <volcaronitee8533@gmail.com>
2023-08-06 19:34:29 -0400

    Added auction API page lbin data

--- flask.py
+++ flask.py
@@ -5,22 +5,19 @@ app = Flask(__name__)
 
 data = {}  # To store the data
 
-
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
     if request.method == 'POST':
         # Get data from the POST request
         data_json = request.get_json()
-        text = data_json.get('input', '')
-        cc = len(text)
+        lbin = data_json.get('lbin', '')
 
         # Store the data with timestamp
-        data_set = {'input': text, 'last_updated': time.time(), 'cc': cc}
+        data_set = {'lbin': lbin, 'last_updated': time.time()}
         data['data'] = data_set
 
     # Return the stored data as JSON
     return jsonify(data.get('data', {}))
-
 
 if __name__ == '__main__':
     app.run()
--- main.py
+++ main.py
@@ -1,23 +1,58 @@
 import requests as rq
-from datetime import datetime
+import json
+import base64
+import gzip
+import io
+from nbtlib import Compound
 
-URL = 'https://volcaronitee.pythonanywhere.com'
+AUCTION_URL = 'https://api.hypixel.net/skyblock/auctions'
+API_URL = 'https://volcaronitee.pythonanywhere.com'
+lbin = {}
+
+
+def get_auction(page):
+    response = rq.get(AUCTION_URL, params={'page': page})
+
+    # Check if the request was successful (status code 200 indicates success)
+    if response.status_code == 200:
+        # The API response is usually in JSON format, so you can parse it like this:
+        data = response.json()
+        print(f'Auction Looping: {page + 1}/{data.get("totalPages")}')
+        for auction in data["auctions"]:
+            if auction['bin']:
+                item_cost = auction['starting_bid']
+
+                # Get Item ID
+                # Decode => Decompress => Warp in io.BytesIO to parse the Base64-encoded data
+                encoded_data = auction["item_bytes"]
+                decoded_data = base64.b64decode(encoded_data)
+                decompressed_data = gzip.decompress(decoded_data)
+                nbt_object = Compound.parse(io.BytesIO(decompressed_data))
+                item_id = str(nbt_object['']['i'][0]['tag']['ExtraAttributes']['id'])
+                if item_id == "PET":
+                    pet_info = json.loads(nbt_object['']['i'][0]['tag']['ExtraAttributes']['petInfo'])
+                    item_id = f'{pet_info["tier"]}_{pet_info["type"]}'
+
+                current = lbin.get(item_id)
+                lbin[item_id] = item_cost if current is None else min(item_cost, lbin.get(item_id))
+        if page + 1 < data['totalPages']:
+            get_auction(page + 1)
+        else:
+            print(f'Auction Loop Complete!')
+    else:
+        print(f"Failed to get data. Status code: {response.status_code}")
+        data = None
+    return data
 
 
 # Function to send data via POST request
 def send_data(data):
-    response = rq.post(URL, json=data)
+    response = rq.post(API_URL, json=data)
     return response.json()
 
 
 if __name__ == "__main__":
-    data_to_send = {'input': 'Hello World!'}  # Replace with your desired data
+    get_auction(0)
+
+    data_to_send = {'lbin': lbin}  # Replace with your desired data
     response_json = send_data(data_to_send)
-
-    rq_input = response_json.get('input', '')
-    updated = datetime.fromtimestamp(response_json.get('last_updated', 0))
-    cc = response_json.get('cc', 0)
-
-    print(f'Input is: {rq_input}')
-    print(f'Last Updated: {updated}')
-    print(f'Character count: {cc}')
